4658d1dcd3c47aa07ad3a3c6bb02ad28ca088433
241046 <241046@std.hi-joho.ac.jp>
2025-05-16 11:58:55 +0900

    java復習課題

---
+++ src/Review4.java
@@ -0,0 +1,13 @@
+public class Review4 {
+    public static void main(String[] args){
+        int number = 5;
+        int str = 88 + 88;
+        final double PI = 3.141592;
+        double circ = 3.14;
+        long a = 100000000;
+        boolean flg = true;
+        byte b = 35;
+        int num = 0*735;
+        char c = 'a';
+    }
+}
