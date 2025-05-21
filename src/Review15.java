b1b0c938d348cbb99ff718ea7eaef6b078162b26
241046 <241046@std.hi-joho.ac.jp>
2025-05-21 10:16:12 +0900

    java復習課題

---
+++ src/Review15.java
@@ -0,0 +1,15 @@
+public class Review15 {
+    public static void main(String[] args) {
+        String A = new java.util.Scanner(System.in).nextLine();
+        try {
+            int num = Integer.parseInt(A);
+            if (num % 2 == 0) {
+                System.out.println("偶数");
+            } else if (num % 2 == 1) {
+                System.out.println("奇数");
+            }
+        } catch (NumberFormatException e){
+            System.out.println("無効な入力です。");
+        }
+    }
+}
