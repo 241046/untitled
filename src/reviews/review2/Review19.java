59d57e01214493b0802ca9cb58f61b8151bd99a4
241046 <241046@std.hi-joho.ac.jp>
2025-05-26 12:30:33 +0900

    java復習課題

---
+++ src/reviews/review2/Review19.java
@@ -0,0 +1,15 @@
+package reviews.review2;
+
+public class Review19 {
+    public static void main(String[] args){
+        int num = 3;
+        int sum = 1;
+        int i = 1;
+        while(i <= num){
+            sum = sum * i;
+            i = i + 1;
+        }
+        System.out.println("結果:" + sum);
+    }
+}
+
